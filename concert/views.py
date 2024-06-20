3d4cb618de31db6ae922ebe87855f496cb6acdc1
PasinduNimesha <r.m.p.n.rathnayake@gmail.com>
2024-06-20 05:32:04 -0400

    added some views

--- concert/views.py
+++ concert/views.py
@@ -14,7 +14,7 @@ import requests as req
 # Create your views here.
 
 def signup(request):
-    pass
+    return render(request, "signup.html", {"form": SignUpForm})
 
 
 def index(request):
@@ -22,15 +22,20 @@ def index(request):
 
 
 def songs(request):
-    # songs = {"songs":[]}
-    # return render(request, "songs.html", {"songs": [insert list here]})
-    pass
+    songs = {"songs":[{"id":1,"title":"duis faucibus accumsan odio curabitur convallis","lyrics":"Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis."}]}
+    return render(request, "songs.html", {"songs": songs["songs"]})
 
 
 def photos(request):
-    # photos = []
-    # return render(request, "photos.html", {"photos": photos})
-    pass
+    photos = [{
+    "id": 1,
+    "pic_url": "http://dummyimage.com/136x100.png/5fa2dd/ffffff",
+    "event_country": "United States",
+    "event_state": "District of Columbia",
+    "event_city": "Washington",
+    "event_date": "11/16/2022"
+}]
+    return render(request, "photos.html", {"photos": photos})
 
 def login_view(request):
     pass
